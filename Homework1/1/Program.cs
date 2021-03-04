2bdf283a0c4614b750b3080be2cb9ea9862d51f2
Bryan20191027 <53924379+Bryan20191027@users.noreply.github.com>
2021-03-04 21:05:23 +0800

    Add files via upload

---
+++ Homework1/1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        /*static void Main(string[] args)
+        {
+            int a;
+            int b;
+            string choice;
+            Console.WriteLine("Enter the first number:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the choice");
+            choice = Console.ReadLine();
+            if (choice == "+")
+            {
+                Console.WriteLine("The Answer is {0}",a + b);
+            }
+            else if (choice == "-")
+            {
+                Console.WriteLine("The Answer is {0}", a - b);
+            }
+            else if (choice == "*")
+            {
+                Console.WriteLine("The Answer is {0}", a * b);
+            }
+            else if (choice == "/")
+            {
+                Console.WriteLine("The Answer is {0}", a / b);
+            }
+            else if (choice == "%")
+            {
+                Console.WriteLine("The Answer is {0}", a % b);
+            }
+            else
+                Console.WriteLine("Wrong Choice");
+        }*/
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            string choice;
+            Console.Write("Enter the first number,second number and the symbol:");
+            string str = Console.ReadLine();
+            string[] strs = str.Split(" ");
+            a = int.Parse(strs[0]);
+            b = int.Parse(strs[1]);
+            choice = strs[2];
+            if (choice == "+")
+            {
+                Console.WriteLine("The Answer is {0}", a + b);
+            }
+            else if (choice == "-")
+            {
+                Console.WriteLine("The Answer is {0}", a - b);
+            }
+            else if (choice == "*")
+            {
+                Console.WriteLine("The Answer is {0}", a * b);
+            }
+            else if (choice == "/")
+            {
+                Console.WriteLine("The Answer is {0}", a / b);
+            }
+            else if (choice == "%")
+            {
+                Console.WriteLine("The Answer is {0}", a % b);
+            }
+            else
+                Console.WriteLine("Wrong Choice");
+        }
+    }
+}
