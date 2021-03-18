aa223cbb564eb9702209fe0535af37319458f8b5
Bryan20191027 <53924379+Bryan20191027@users.noreply.github.com>
2021-03-18 12:25:57 +0800

    Add files via upload

---
+++ Homework4/4/4.1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+
+namespace _4
+{
+    
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public Node<T> Tail
+        {
+            get => tail;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        
+        public static void ForEachNode(GenericList<T> l,Action<T> action)
+        {
+            Node<T> top = l.Head;
+            while (top != null)
+            {
+                action(top.Data);
+                top = top.Next;
+            }
+        }
+
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> g = new GenericList<int>();
+            g.Add(2);
+            g.Add(3);
+            g.Add(4);
+            GenericList<int>.ForEachNode(g,(n)=> { Console.WriteLine(n); });
+            int max = g.Head.Data;
+            int min = g.Head.Data;
+            int sum = 0;
+            GenericList<int>.ForEachNode(g, (n) => {
+                max = Math.Max(n, max);
+            });
+            Console.WriteLine($"the max is {max}");
+            GenericList<int>.ForEachNode(g, (n) => { min = Math.Min(n, min); });
+            Console.WriteLine($"the min is {min}");
+            GenericList<int>.ForEachNode(g, (n) => { sum += n; });
+            Console.WriteLine($"the sum is {sum}");
+        }
+    }
+}
---
+++ Homework4/4/4.2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace _4._2
+{
+    public delegate void ClockHandler(object sender, ClockArgs args);
+
+    public class ClockArgs{
+        public string Hour { get; set; }
+        public string Minute { get; set; }
+        public string Second { get; set; }
+    }
+
+    public class Clock
+    {
+        private int hour, minute, second, hourSet, minuteSet, secondSet;
+        public event ClockHandler Tick;
+        public event ClockHandler Alarm;
+        public Clock()
+        {
+            hour = int.Parse(DateTime.Now.Hour.ToString());
+            minute = int.Parse(DateTime.Now.Minute.ToString());
+            second = int.Parse(DateTime.Now.Second.ToString());
+        }
+
+        public void SetAlarm(int hour, int minute,int second)
+        {
+            hourSet = hour;
+            minuteSet = minute;
+            secondSet = second;
+        }
+
+        public void SetAlarm(int hour, int minute)
+        {
+            hourSet = hour;
+            minuteSet = minute;
+            secondSet = 0;
+        }
+
+        public void RunClock()
+        {
+            while (true)
+            {
+                this.hour = int.Parse(DateTime.Now.Hour.ToString());
+                this.minute = int.Parse(DateTime.Now.Minute.ToString());
+                this.second = int.Parse(DateTime.Now.Second.ToString());
+                ClockArgs TimeNow = new ClockArgs() { Hour = hour.ToString(), Minute = minute.ToString(), Second = second.ToString() };
+                if (TimeNow.Hour.Length == 1)
+                    TimeNow.Hour = "0" + TimeNow.Hour;
+                if (TimeNow.Minute.Length == 1)
+                    TimeNow.Minute = "0"+ TimeNow.Minute;
+                if (TimeNow.Second.Length == 1)
+                    TimeNow.Second = "0"+ TimeNow.Second;
+                if (this.hour == this.hourSet && this.minute == this.minuteSet && this.second == this.secondSet)
+                {
+                    Alarm(this, TimeNow);
+                }
+                else
+                    Tick(this, TimeNow);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public class NewClock
+    {
+        public Clock ClockTemp;
+        public NewClock()
+        {
+            ClockTemp = new Clock();
+            ClockTemp.Tick += Tick;
+            ClockTemp.Alarm += Alarm;
+        }
+        void Tick(object sender, ClockArgs args)
+        {
+            Console.WriteLine("TickEvent：" + args.Hour + ":" + args.Minute + ":" + args.Second);
+        }
+        void Alarm(object sender, ClockArgs args)
+        {
+            Console.WriteLine("AlarmEvent:" + args.Hour + ":" + args.Minute + ":" + args.Second);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewClock Clock = new NewClock();
+            Console.WriteLine("Enter the time to set:");
+            string str = Console.ReadLine();
+            string[] strs = str.Split(":");
+            if(strs.Length == 2)
+            {
+                Clock.ClockTemp.SetAlarm(int.Parse(strs[0].ToString()), int.Parse(strs[1].ToString()));
+            }
+            else
+                Clock.ClockTemp.SetAlarm(int.Parse(strs[0].ToString()), int.Parse(strs[1].ToString()), int.Parse(strs[2].ToString()));
+            Clock.ClockTemp.RunClock();
+        }
+    }
+}
