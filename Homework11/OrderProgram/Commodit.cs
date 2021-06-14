5895756aa83d4f7d621619747fba16150612f82c
Bryan20191027 <53924379+Bryan20191027@users.noreply.github.com>
2021-06-14 12:50:10 +0800

    Add files via upload

---
+++ Homework11/OrderProgram/Commodit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OrderProgram
+{
+    public class Commodit
+    {
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+
+        public override string ToString()
+        {
+            return "货物种类：" + Name + " 货物单价：" + UnitPrice;
+        }
+        public Commodit(string Name, double unitPrice)
+        {
+            this.Name = Name;
+            this.UnitPrice = unitPrice;
+        }
+
+        public Commodit()
+        {
+
+        }
+    }
+}
---
+++ Homework11/OrderProgram/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProgram
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Time { get; set; }
+        public Customer()
+        {
+            this.Time = DateTime.Now.ToString();
+        }
+        public Customer(string Name, string Address)
+        {
+            this.Time = DateTime.Now.ToString();
+            this.Name = Name;
+            this.Address = Address;
+        }
+
+        public override string ToString()
+        {
+            return "客户名字：" + Name + " 收货地址：" + Address + " 下单时间：" + Time;
+        }
+    }
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext() : base("CustomerDataBase")
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<OrderContext>());
+        }
+
+        public DbSet<Customer> customers { get; set; }
+    }
+}
